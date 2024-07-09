e6331853d4adeafb04d36db6612aa11a91bcdf4c
Tulasi Reddy <tulasireddy.kagithala@karkinos.in>
2024-07-10 00:48:30 +0530

    Fixed new state not emitting issue

--- lib/app/view/qr_scanner/bloc/barcode_scanner_bloc.dart
+++ lib/app/view/qr_scanner/bloc/barcode_scanner_bloc.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
-import 'dart:developer';
+import 'dart:developer' as logg;
+import 'dart:math';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
 import 'package:meta/meta.dart';
 import 'package:universal_cart/app/model/cart_model.dart';
 import 'package:universal_cart/cart_repository.dart';
@@ -36,10 +38,18 @@ class BarcodeScannerBloc extends Bloc<BarcodeScannerEvent, BarcodeScannerState> 
 
       if (products.isNotEmpty) {
         Product foundProduct = products.first;
-        log('Product found: ${foundProduct.name}, Price: ${foundProduct.price}');
-        emit(HomeNavigateState(product: foundProduct));
+        /// Here we are Random key for to emit new state,
+        /// if product object was same new state will not emit
+        var key = Random();
+        final String keyVal = key.nextInt(100000).toString();
+        logg.log('Product found: ${foundProduct.name}, Price: ${foundProduct.price}, key: $keyVal');
+        Product foundProduct1 = Product(barcode: foundProduct.barcode, totalPrice: foundProduct.totalPrice, price: foundProduct.price,
+        brand: foundProduct.brand, description: foundProduct.description, discount: foundProduct.discount, finalPrice: foundProduct.finalPrice, inventoryStatus: keyVal,
+        location: foundProduct.location, name: foundProduct.name, productId: foundProduct.productId, weight: foundProduct.weight, quantity: foundProduct.quantity,
+        paymentMethod: foundProduct.paymentMethod);
+        emit(HomeNavigateState(product: foundProduct1));
       } else {
-        log('Product not found');
+        logg.log('Product not found');
       }
     } catch (e) {
       emit(ItemError());
